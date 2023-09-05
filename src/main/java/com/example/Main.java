f90146d6ac93cc94ee4205004b306a3dc928f8a2
kubad <kubadralus@gmail.com>
2023-09-05 18:25:08 +0200

    working!

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,14 @@
 package com.example;
 
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String token = args[0];
+        JDA jda = JDABuilder.createDefault(token).build();
+        
+        System.out.println("success!");
     }
 }
